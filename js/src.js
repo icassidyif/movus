61dcfed4b74b9a01f94f20aa363e46c20990946b
Cassidy <icassidyif@gmail.com>
2019-01-28 16:19:11 +0200

    28012019_1

--- js/src.js
+++ js/src.js
@@ -2,6 +2,8 @@
 var mainMenu = document.querySelector('.main-menu');
 var subMenus = document.querySelectorAll('.sub-menu');
 
+var menuOpener = document.querySelector('.main-menu__opener');
+
 for (var i = 0; i < subMenus.length; i++) {
   var currentElement = subMenus[i];
   currentElement.previousElementSibling.addEventListener("click", function () {
@@ -10,3 +12,6 @@ for (var i = 0; i < subMenus.length; i++) {
   })
 }
   
+menuOpener.addEventListener('click',function(){
+  mainMenu.classList.toggle('expanded');
+})

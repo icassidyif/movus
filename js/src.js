4964e2fbe93507062f4b4279ad1ff0acae925a10
Cassidy <icassidyif@gmail.com>
2019-02-01 19:01:57 +0200

    01022019_3

--- js/src.js
+++ js/src.js
@@ -1,17 +1,17 @@
-// Expand subMenu-----------------------------------------
-var mainMenu = document.querySelector('.main-menu');
-var subMenus = document.querySelectorAll('.sub-menu');
+// // Expand subMenu-----------------------------------------
+// var mainMenu = document.querySelector('.main-menu');
+// var subMenus = document.querySelectorAll('.sub-menu');
 
-var menuOpener = document.querySelector('.main-menu__opener');
+// var menuOpener = document.querySelector('.main-menu__opener');
 
-for (var i = 0; i < subMenus.length; i++) {
-  var currentElement = subMenus[i];
-  currentElement.previousElementSibling.addEventListener("click", function () {
-  currentElement.classList.toggle('expanded');
-  currentElement.previousElementSibling.classList.toggle('is-active');
-  })
-}
+// for (var i = 0; i < subMenus.length; i++) {
+//   var currentElement = subMenus[i];
+//   currentElement.previousElementSibling.addEventListener("click", function () {
+//   currentElement.classList.toggle('expanded');
+//   currentElement.previousElementSibling.classList.toggle('is-active');
+//   })
+// }
   
-menuOpener.addEventListener('click',function(){
-  mainMenu.classList.toggle('expanded');
-})
+// menuOpener.addEventListener('click',function(){
+//   mainMenu.classList.toggle('expanded');
+// })
